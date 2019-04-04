0f575e4fddf1b97519967eafea6d0592c3032b1b
dahai <1559843332@qq.com>
2019-04-04 18:08:27 +0800

    init vue

---
+++ src/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <input v-model="inputValue"/>
+    <button @click="tianjia">添加</button>
+    <ul>
+      <!-- <li v-for="(item,index) in list" v-key="index">{{item}}</li> -->
+      <todo-item  v-for="(item,index) in list" :key="item" :content="item" :index="index" @zidel="delMethod"></todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  components:{
+    'todo-item':TodoItem
+  },
+  data(){
+    return {
+      inputValue:"dahai",
+      list:[]
+    }
+  },
+  methods:{
+    tianjia(){
+      this.list.push(this.inputValue);
+    },
+    delMethod(index){
+      this.list.splice(index,1);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li @click="del">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props:['content','index'],
+  methods:{
+    del(){
+      this.$emit('zidel',this.index);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
